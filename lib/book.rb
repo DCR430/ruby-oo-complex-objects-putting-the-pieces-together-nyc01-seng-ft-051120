25a037de33a5746a67701ccf87e8f1699965f93b
DCR430 <daniel.c.reyes30@gmail.com>
2020-06-03 09:36:45 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,41 +1,56 @@
 class Book
-    def initialize(title)
+    def initialize (title)
         @title = title
     end
-    
+
     def title
         @title
     end
 
-    def author=(author)
-        @author = author
+    def author=(author_name)
+        @author = author_name
     end
 
     def author
         @author
     end
 
-    def page_count=(num)
-        @page_count = num
+    def page_count=(page_count)
+        @page_count = page_count
     end
 
     def page_count
         @page_count
     end
-    
-    def genre=(genre)
-        @genre = genre
+
+    def genre=(new_genre)
+        @genre = new_genre
     end
 
     def genre
         @genre
     end
 
+
     def turn_page
         puts "Flipping the page...wow, you read fast!"
     end
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
 
 Book.new("And Then There Were None")
-
